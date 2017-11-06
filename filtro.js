65ec84ac0b9025f016b7390a6cbd3eaecfedc916
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2017-11-05 21:34:43 -0300

    Filtro operando combinadamente. La implementacion esta muy acoplada al DOM y depende de ella por lo que es sucia. Esto deberia poder modificarse apelando a MVC. Por hacer

--- filtro.js
+++ filtro.js
@@ -1,19 +1,22 @@
 console.log("Filtro");
 
-var estado = {
-  nombre: {
-    estado: undefined,
-    active: false
+var estado = [
+  {
+    identificador: "nombres",
+    valor: undefined,
+    activado: false
   },
-  color: {
-    estado: undefined,
-    active: false
+  {
+    identificador: "colores",
+    valor: undefined,
+    activado:false
   },
-  booleano: {
-    estado: undefined,
-    active: false
+  {
+    identificador: "booleano",
+    valor: undefined,
+    activado: false
   }
-}
+]
 
 // Datos.
 var datos = [
@@ -67,7 +70,7 @@ var datos = [
     data_booleano: false
   },
   {
-    id: "nicolas",
+    id: "camilo",
     nombre: "Camilo",
     data_tipo:"lista",
     data_color: "azul",
@@ -139,58 +142,87 @@ var datos = [
 ]
 
 function inyectData(datos){
-  let contenedor = document.getElementById('contenido');
+  let contenedor = document.querySelector('#contenido')
+  let parrafo = document.querySelector('#p_texto')
+  if(parrafo !== null){
+    //console.log(parrafo);
+    parrafo.remove()
+  }
+  // inspeccionar el contenedor
   let lista_ordenada = document.createElement('ol')
   contenedor.appendChild(lista_ordenada)
   for(let i = 0; i < datos.length; i++){
     let li = document.createElement('li')
-    li.textContent = datos[i].nombre
-    li.setAttribute('data_tipo', datos[i].data_tipo)
+    li.textContent = `nombre : ${datos[i].nombre.toLowerCase()} - color: ${datos[i].data_color.toLowerCase()} - booelano: ${datos[i].data_booleano}`
+    li.dataset["tipo"] =  datos[i].data_tipo
     li.setAttribute('id', datos[i].id)
-    li.setAttribute('data_color', datos[i].data_color)
-    li.setAttribute('data_booleano', datos[i].data_booleano)
+    li.dataset["color"] = datos[i].data_color
+    li.dataset["booleano"] = datos[i].data_booleano
+    li.dataset["nombre"] = datos[i].nombre.toLowerCase()
     lista_ordenada.appendChild(li)
   }
+  console.log(contenedor.children.length);
 }
 
-function changeOptions(new_estado, datos, arr){
-  console.table(new_estado)
-  console.log("Arreglo");
-  console.log(arr);
-  if(arr.length > 0){
-    arr = arr.filter((e) => new_estado.booleano.estado === e.data_booleano)
+function resetFiltros(){
+  let selects = document.querySelectorAll('select')
+  //console.log(selects);
+  for(let i = 0; i < selects.length; i++){
+    selects[i].selectedIndex = 0;
   }
-  else if (arr.length === 0){
-    arr = datos.filter((elemento) => new_estado.color.estado === elemento.data_color || new_estado.booleano.estado === elemento.data_booleano)
-  }
-  return arr
+  //new_estado = ["Nombre", "Color", "Booleano"]
+  let ol = document.querySelector('ol')
+  ol.remove();
+  inyectData(datos)
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("Script cuando carga el contenido");
   inyectData(datos);
 
+  let new_data = datos
+  let new_estado = ["Nombre", "Color", "Booleano"]
+
   let selects = document.querySelectorAll('select');
-  selects.forEach((elem) => {
-    new_data = datos
-    let arr = []
-    elem.addEventListener('change', (evento) => {
-      let new_estado = estado
-      if(evento.target.id === "nombres"){
-        new_estado.nombre.estado = evento.target.value.toLowerCase()
-        new_estado.nombre.active = true
+  for(let i = 0; i < selects.length; i++){
+    selects[i].addEventListener('change', (e) => {
+      new_estado[Number(selects[i].dataset["numero"])-1] = e.target.value.toLowerCase()
+      // console.log(new_estado);
+      let elementos = document.querySelectorAll('li')
+      let cantidad = elementos.length
+      for(let i = 0; i < elementos.length; i++){
+        //console.log(i);
+        let elemento = elementos[i]
+        let nombre = elemento.dataset["nombre"]
+        let color = elemento.dataset["color"]
+        let booleano = elemento.dataset["booleano"]
+        // console.log(new_estado);
+        if((nombre === new_estado[0] || new_estado[0] === "Nombre") && (color === new_estado[1] || new_estado[1] === "Color") && (booleano === new_estado[2] || new_estado[2] === "Booleano")){
+          elemento.style.display = "block"
+        }
+        else if (document.querySelectorAll('li[style="display: none;"]').length === cantidad){
+          // resetFiltros()
+          let contenido = document.querySelector('#contenido')
+          let parrafo = document.createElement('p')
+          parrafo.setAttribute('id', "p_texto")
+          parrafo.textContent = "No hay mas elementos que mostrar. Click en Reset!";
+          contenido.appendChild(parrafo)
+          new_estado = ["Nombre", "Color", "Booleano"]
+          console.log(new_estado);
+          break;
+        }
+        else{
+          elemento.style.display = "none"
+        }
       }
-      else if (evento.target.id === "colores"){
-        new_estado.color.estado = evento.target.value.toLowerCase()
-        new_estado.color.active = true
-      }
-      else if (evento.target.id === "booleano"){
-        new_estado.booleano.estado = evento.target.value === "True" ? true : false
-        new_estado.booleano.active = true
-      }
-      //console.table(new_estado)
-      new_data = changeOptions(new_estado, datos, arr)
-      console.table(new_data)
     })
+  }
+
+  let boton = document.querySelector('#reset_filtro')
+  boton.addEventListener('click', (e) => {
+    resetFiltros()
+    new_estado = ["Nombre", "Color", "Booleano"]
+    console.log(new_estado);
   })
 })
